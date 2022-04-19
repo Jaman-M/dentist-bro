e0701bb35a66f624faffadf72c6ddf2dd7ca7fa0
Jaman-M <jaman.manik1@gmail.comm>
2022-04-19 16:21:30 +0600

    auth redirect, signout

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const {serviceId} = useParams();
     return (
         <div>
             <h2>service Detail: {serviceId}</h2>
+            <div className='text-center'>
+            <Link to={'/checkout'}>
+                <button className='btn btn-info'> Checkout</button>
+            </Link>
+            </div>
         </div>
     );
 };
