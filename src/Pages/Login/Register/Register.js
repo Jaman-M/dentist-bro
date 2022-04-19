79f01ad0b4f1e4a072d73afdcb45ce160be3f6f0
Jaman-M <jaman.manik1@gmail.comm>
2022-04-19 14:25:40 +0600

    added register form set rout

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
     return (
-        <div>
+        <div className='register-form'>
             <h2>Please register</h2>
+            <form action="">
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your email Address' required/>
+                <input type="password" name="password" id="" placeholder='password' required/>
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account ? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
