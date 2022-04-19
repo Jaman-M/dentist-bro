7f04839ced4595c0482a6284c1ba0e246f7e9324
Jaman-M <jaman.manik1@gmail.comm>
2022-04-19 15:37:55 +0600

    create firebase hooks, create userWithEmailAndPassword, useSignInWithEmailAndPassword --Authentication

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Register.css'
+import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init'
+
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login');
     }
+
+    if (user){
+        navigate('/home');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        // console.log(event.target.email.value);
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password)
+    }
+
     return (
         <div className='register-form'>
             <h2>Please register</h2>
-            <form action="">
+            <form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Your email Address' required/>
                 <input type="password" name="password" id="" placeholder='password' required/>
